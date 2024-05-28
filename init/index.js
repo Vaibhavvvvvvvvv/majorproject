8140a59a5058aa2c8191fd91410aa29ada2bf182
Vaibhavvvvvvvvv <777vsolanki@gmail.com>
2024-05-28 12:31:42 +0530

    saved_img_in_cloud

--- init/index.js
+++ init/index.js
@@ -6,25 +6,33 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log("Connected to DB");
   })
   .catch((err) => {
-    console.log(err);
+    console.error(err);
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 }
 
 const initDB = async () => {
-  // Adjust data to ensure `image` is a string
-  const adjustedData = initData.data.map(listing => ({
-    ...listing,
-    image: listing.image.url, // Use the `url` field as the image string
-  }));
+  // Adjust data to ensure `image` is a string and log the adjusted data
+  const adjustedData = initData.data.map(listing => {
+    console.log('Original listing:', listing);
+    const adjustedListing = {
+      ...listing,
+      image: listing.image && listing.image.url ? listing.image.url : 'https://th.bing.com/th/id/R.e935816b4c7521bcf1c27b319c1bd92e?rik=ia9rc0iCI%2bpDKQ&riu=http%3a%2f%2fwww.telegraph.co.uk%2fcontent%2fdam%2fTravel%2fhotels%2fcaribbean-islands%2fst-lucia%2fvilla-beach-cottages-st-lucia-pool.jpg&ehk=RZNfNEMlx76BcHOhWwD674m1XqrtKxZWw2cpdCYsW8w%3d&risl=&pid=ImgRaw&r=0', // Use the `url` field as the image string or a default value
+    };
+    console.log('Adjusted listing:', adjustedListing);
+    return adjustedListing;
+  });
   
   await Listing.deleteMany({});
-  await Listing.insertMany(adjustedData); // Insert adjusted data
+  await Listing.insertMany(adjustedData);
   console.log("Data was initialized");
 };
 
